3065d8c5f6cb7cccb84756e702bcd436d775da24
Matthew Esqueda <matthewjw2007@yahoo.com>
2019-06-13 22:08:55 -0700

    added my first component

---
+++ components/src/CommentDetail.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import faker from "faker";
+
+const CommentDetail = () => {
+  return (
+      <div className={"comment"}>
+          <a href={"/"} className={"avatar"}>
+              <img alt={"avatar"} src={faker.image.avatar()} />
+          </a>
+          <div className={"content"}>
+              <a href={"/"} className={"author"}>
+                  Sam
+              </a>
+              <div className={"metadata"}>
+                  <span className={"date"}>Time 6:00PM</span>
+              </div>
+              <div className={"text"}>Nice blog post!</div>
+          </div>
+      </div>
+  );
+};
+
+// Needed to allow another file access to this component
+export default CommentDetail;
