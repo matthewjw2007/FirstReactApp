9713b43aadf40e38d19ad017ce7a51aff0d5d15e
Matthew Esqueda <matthewjw2007@yahoo.com>
2019-06-14 22:37:52 -0700

    working with props

--- components/src/index.js
+++ components/src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
     return (
         <div className={"ui container comments"}>
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <CommentDetail
+                avatar={faker.image.avatar()}
+                author={"Sam"}
+                timeAgo={"Today at 4:45PM"}
+                content={"Nice blog post!"}
+            />
+            <CommentDetail
+                avatar={faker.image.avatar()}
+                author={"Alex"}
+                timeAgo={"Today at 2:00AM"}
+                content={"I like the subject"}
+            />
+            <CommentDetail
+                avatar={faker.image.avatar()}
+                author={"Jane"}
+                timeAgo={"Yesterday at 5:00PM"}
+                content={"I like the writing"}
+            />
         </div>
     );
 };
