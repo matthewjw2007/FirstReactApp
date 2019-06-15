6cf80d27fdc58572a81d9a0df2a490227839ae24
Matthew Esqueda <matthewjw2007@yahoo.com>
2019-06-14 22:53:57 -0700

    Passing a child component to a parent component

--- components/src/index.js
+++ components/src/index.js
@@ -1,29 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 import faker from 'faker';
 
 const App = () => {
     return (
         <div className={"ui container comments"}>
-            <CommentDetail
-                avatar={faker.image.avatar()}
-                author={"Sam"}
-                timeAgo={"Today at 4:45PM"}
-                content={"Nice blog post!"}
-            />
-            <CommentDetail
-                avatar={faker.image.avatar()}
-                author={"Alex"}
-                timeAgo={"Today at 2:00AM"}
-                content={"I like the subject"}
-            />
-            <CommentDetail
-                avatar={faker.image.avatar()}
-                author={"Jane"}
-                timeAgo={"Yesterday at 5:00PM"}
-                content={"I like the writing"}
-            />
+            <ApprovalCard>
+                <CommentDetail
+                    avatar={faker.image.avatar()}
+                    author={"Sam"}
+                    timeAgo={"Today at 4:45PM"}
+                    content={"Nice blog post!"}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    avatar={faker.image.avatar()}
+                    author={"Alex"}
+                    timeAgo={"Today at 2:00AM"}
+                    content={"I like the subject"}
+                />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail
+                    avatar={faker.image.avatar()}
+                    author={"Jane"}
+                    timeAgo={"Yesterday at 5:00PM"}
+                    content={"I like the writing"}
+                />
+            </ApprovalCard>
         </div>
     );
 };
