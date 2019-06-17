10048e8cff324c292fb945c46daf507aebdf3d86
Matthew Esqueda <matthewjw2007@yahoo.com>
2019-06-16 20:02:05 -0700

    Updated to have conditional rendering

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -8,20 +8,29 @@ class App extends React.Component {
         super(props);
 
         // THIS IS THE ONLY TIME YOU DO DIRECT ASSIGNMENT TO this.state
-        this.state = {lat: null};
+        this.state = {lat: null, errorMessage: '' };
 
         window.navigator.geolocation.getCurrentPosition(
             // We called setState to update the state object
             (position) => {
-                this.setState({lat: position.coords.latitude})
+                this.setState({lat: position.coords.latitude});
             },
-            (err) => {console.log(err)}
+            (err) => {
+                this.setState({errorMessage: err.message});
+            }
         );
     }
 
     // React says we have to define render!!!
     render() {
-        return <div>Latitude: {this.state.lat}</div>
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        else if(this.state.lat && !this.state.errorMessage){
+            return <div>Latitude: {this.state.lat}</div>
+        } else {
+            return <div>Loading...</div>
+        }
     }
 }
 
